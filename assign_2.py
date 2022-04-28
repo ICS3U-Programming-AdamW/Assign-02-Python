751a0da68f92830ff6de450e8ee2c9c34a4623fd
Ubuntu <ubuntu@ip-172-31-2-252.ec2.internal>
2022-04-28 18:07:00 +0000

    push

--- assign_2.py
+++ assign_2.py
@@ -3,7 +3,7 @@
 # Created by: Adam Winogron
 # Created on: April 23, 2022
 # This program asks the user for a length
-# and height and calculates the surface area and volume 
+# and height and calculates the surface area and volume
 # of a square-based pyramid with those dimensions.
 
 # include math
@@ -11,53 +11,81 @@ import math
 
 
 def main():
-    
-    #Describe program function
-    print("If you give me the dimensions I need I'll calculate\
- the surface area and volume of a square-based pyramid!!")
+
+    # Describe program function
+    print("If you give me the dimensions I need, I'll calculate \
+the surface area and volume of a square-based pyramid!!")
     print("")
+
+    # declare variables
+    length_string = ""
+    height_string = ""
+    units = ""
 
     # Catch statement
     try:
         # Ask user for length of base
-        length_string = input("the first thing I need is the length of the square base:")
-        length = int(length_string)
-        print("")
-
-        # Ask user for height of pyramid
-        height_string = input("next I'll need the height of the pyramid!:")
-        height = int(height_string)
-        print("")
-
-        # calculate the base area
-        base_area = (length**2)
-
-        # calculate the side area
-        pythag = (height**2 + ((length / 2) ** 2))
-        side_height = (math.sqrt(pythag))
-        side_area = ((side_height * length) / 2)
-
-        # calculate the volume
-        volume = ((height / 3) * base_area)
-
-        # calculata the surface area
-        surface_area = (base_area + (side_area * 4)
-
-        # print the surface area
-        print("the surface area of your pyramid is {} units!".format(surface_area))
-        print("")
-
-        # print the volume
-        print("the volume of your pyramid is {} units cubed!".format(volume))
+        length_string = input("The first thing I need is the\
+ length of the square base: ")
+        length = float(length_string)
         print("")
 
     except Exception:
-        #tell the user their input wasn't an interger
-        print("It seems that '{}' won't work! I need a number to do math!")
+        # tell the user their input wasn't an interger
+        print()
+        print("It seems that '{}' won't work! I need \
+a number to do math!".format(length_string))
 
-    finally:
-        print("Restart the program and give me some new numbers!")
+    else:
+        try:
+            # Ask user for height of pyramid
+            height_string = input("Next I'll need the height \
+of the pyramid!: ")
+            height = float(height_string)
+            print("")
+
+            # ask user for the units
+            units = input("The last thing I'll need is the units that this shape \
+will be defined by (cm, in, m, etc.): ")
+            print("")
+
+    # MATH
+            # calculate the base area
+            base_area = (length**2)
+
+            # calculate the side area
+            pythag = (height**2 + ((length / 2) ** 2))
+            side_height = (math.sqrt(pythag))
+            side_area = ((side_height * length) / 2)
+
+            # calculate the volume
+            volume = ((height / 3) * base_area)
+
+            # calculata the surface area
+            surface_area = (base_area + (side_area * 4))
+
+            # print the surface area
+            print("The surface area of your pyramid is {:.2f} \
+{}!".format(surface_area, units))
+            print("")
+
+            # print the volume
+            print("The volume of your pyramid is {:.2f} \
+{} cubed!".format(volume, units))
+            print("")
+
+        except Exception:
+            # tell the user their input wasn't an interger
+            print()
+            print("It seems that '{}' won't work! \
+I need a number to do math!".format(height_string))
+
+        finally:
+            # tell user to restart
+            print()
+            print("Restart the program and give me some new numbers!")
 
 
+# call main()
 if __name__ == "__main__":
     main()
